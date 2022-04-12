2af635daf2b37c12504985226a904efd4509b60d
huadong.chen <huadong.chen@shopee.com>
2022-04-12 14:19:53 +0800

    feat: add route generator

--- demos/design-patterns/depdency-injection/app.ts
+++ demos/design-patterns/depdency-injection/app.ts
@@ -1,7 +1,11 @@
 import http, { RequestListener } from 'http'
 import { UserModule } from './user.module'
 import { UserController } from './user.controller'
+import { generateRoutes } from './helpers/generateRoutes'
+import { UserService } from './user.service'
 
+const routes = generateRoutes(new UserController(new UserService()))
+console.log('routes:', routes)
 const handleHttpListen: RequestListener = (request, response) => {
   request.method
 	response.end('ok')
--- demos/design-patterns/depdency-injection/helpers/generateRoutes.ts
+++ demos/design-patterns/depdency-injection/helpers/generateRoutes.ts
@@ -1,8 +1,24 @@
 import 'reflect-metadata'
-import { PATH_METADATA, METHOD_METADATA, METHODS } from '../constants/metadata'
+import { PATH_METADATA, METHOD_METADATA } from '../constants/metadata'
+import { isFunction } from 'util'
 
 export const generateRoutes = (instance: Object) => {
+  console.log('instance:', instance)
   const globalPath = Reflect.getMetadata(PATH_METADATA, instance)
-  // TODO
-  // const methodNames = Object.getOwnPropertyNames()
+
+  const prototype = Object.getPrototypeOf(instance)
+  const methodNames = Object.getOwnPropertyNames(instance)
+    .filter(name => isFunction(prototype[name]))
+  
+  return methodNames.map(methodName => {
+    const fn = prototype[methodName]
+    const path = Reflect.getMetadata(PATH_METADATA, fn)
+    const method = Reflect.getMetadata(METHOD_METADATA, fn)
+
+    return {
+      path: `${globalPath}/${path}`,
+      method,
+      handler: fn
+    }
+  })
 }
