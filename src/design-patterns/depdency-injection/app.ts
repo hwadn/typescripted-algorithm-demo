cf53f0eb292dae41c068f7564fdad127df9cef21
IamHuadong <1761339618@qq.com>
2022-08-25 22:28:08 +0800

    fix: eslint

--- src/design-patterns/depdency-injection/app.ts
+++ src/design-patterns/depdency-injection/app.ts
@@ -13,15 +13,15 @@ const user = container.get<UserController>(UserController.name)
 const routes = generateRoutes(user)
 
 const handleHttpListen: RequestListener =async (request, response) => {
-  const { method, url } = request
-  const route = routes.find(item => item.method === method && item.path === url)
+	const { method, url } = request
+	const route = routes.find(item => item.method === method && item.path === url)
 
-  if (route) {
-    const handler = route?.handler
-    const data = await handler()
-    response.end(JSON.stringify(data))
-  }
-  response.end()
+	if (route) {
+		const handler = route?.handler
+		const data = await handler()
+		response.end(JSON.stringify(data))
+	}
+	response.end()
 }
 http.createServer(handleHttpListen).listen(4000)
 console.log('server is running on 4000')
--- src/design-patterns/depdency-injection/decorators/inject.decorator.ts
+++ src/design-patterns/depdency-injection/decorators/inject.decorator.ts
@@ -4,17 +4,17 @@ import { INJECT_METADATA } from '../constants/metadata'
 
 // TODOc understand
 export const Inject = (): ParameterDecorator => {
-	return (target: Object, targetKey: any) => {
-    const annotationTarget = target.constructor
-    let props: Record<string, any> = {}
-    if (Reflect.hasOwnMetadata(INJECT_METADATA, annotationTarget)) {
-      props = Reflect.getMetadata(INJECT_METADATA, annotationTarget)
-    }
+	return (target: Record<string, any>, targetKey: any) => {
+		const annotationTarget = target.constructor
+		let props: Record<string, any> = {}
+		if (Reflect.hasOwnMetadata(INJECT_METADATA, annotationTarget)) {
+			props = Reflect.getMetadata(INJECT_METADATA, annotationTarget)
+		}
 
-    props[targetKey] = {
-      value: targetKey
-    }
+		props[targetKey] = {
+			value: targetKey
+		}
 
-    Reflect.defineMetadata(INJECT_METADATA, props, annotationTarget)
+		Reflect.defineMetadata(INJECT_METADATA, props, annotationTarget)
 	}
 }
